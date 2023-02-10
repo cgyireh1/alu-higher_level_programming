710bd41b98550409dc2322a065717961d25c7d78
cgyireh1 <c.gyireh@alustudent.com>
2023-02-10 04:12:54 -0800

    Uppercase

---
+++ python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+    #!/usr/bin/python3
+    def uppercase(str):
+        for x in range(len(str)):
+            if ord(str[x]) >= ord('a') and ord(str[x]) <= ord('z'):
+                atoA = ord(str[x]) + (ord('A') - ord('a'))
+            else:
+                atoA = ord(str[x])
+                print("{}".format(chr(atoA)), end='')
+                print()
