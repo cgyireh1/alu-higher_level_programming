fa998d97fa4b52be22684d918e8391355d405276
cgyireh <c.gyireh@alustudent.com>
2023-02-10 10:55:04 -0800

    Uppercase

---
+++ python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+__import__('10-add')
+
+print(add(-5, -5))
+print(add(98, 2))
+print(add(100, 50)) 
--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for x in range(len(str)):
-        if ord(str[x] >= ord('a') and ord(str[x]) <= ord('z'):
-                atoA = ord(str[x]) + (ord('A') - ord('a'))
+        if ord(str[x]) >= ord('a') and ord(str[x]) <= ord('z'):
+            atoA = ord(str[x]) + (ord('A') - ord('a'))
         else:
-            atoA = ord(str[a])
-        print("{}".format(chr(atoA)), end='')
-    print()
-            
+            atoA = ord(str[x])
+            print("{}".format(chr(atoA)), end='')
+            print()
