907d12f3c97ba82890ea6046416d227a71bfd344
cgyireh <c.gyireh@alustudent.com>
2023-03-08 17:48:40 -0800

    Safe list printing

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -4,7 +4,7 @@ def safe_print_list(my_list=[], x=0):
     Args:
         my_list(list): The list to print elements from.
         x(int): The number of elements of my_list to print.
-    Returns: 
+    Returns:
         The number of Elements printed.
     """
     ret = 0
