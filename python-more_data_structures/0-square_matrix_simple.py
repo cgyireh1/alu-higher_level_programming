034c8ae889e56365c69df5ddb0e5ea4a942925fb
cgyireh <c.gyireh@alustudent.com>
2023-03-09 08:41:41 -0800

    Squared simple

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -3,6 +3,6 @@ def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
     for i in range(len(matrix)):
-        new_matrix[i] = list(map(lambda 1: 1**2, matrix[i]))
+        new_matrix[i] = list(map(lambda i: 1**2, matrix[i]))
 
     return(new_matrix)
