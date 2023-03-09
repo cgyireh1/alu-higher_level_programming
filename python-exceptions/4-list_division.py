9408b28e493245bdfa8423eb7ec70120ed7672c2
cgyireh <c.gyireh@alustudent.com>
2023-03-09 02:11:13 -0800

    Divide a list

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,7 +15,7 @@ def list_division(my_list_1, my_list_2, list_length):
         except TypeError:
             print("wrong type")
             div = 0
-        except  ZeroDivisionError:
+        except ZeroDivisionError:
             print("division by 0")
             div = 0
         except IndexError:
