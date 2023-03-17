021f82a4f3257b76f752c0a7dcf055819b566556
cgyireh <c.gyireh@alustudent.com>
2023-03-17 08:55:28 -0700

    A square is a rectangle

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -85,8 +85,7 @@ class Rectangle:
         """ Prints rectangle with #'s """
         if self.__width == 0 or self.__height == 0:
             return ""
-        pic = "\n".join([str(self.print_symbol) * self.__width
-            for rows in range(self.__height)])
+        pic = "\n".join([str(self.print_symbol) * self.__width for rows in range(self.__height)])
         return pic
 
     def __repr__(self):
