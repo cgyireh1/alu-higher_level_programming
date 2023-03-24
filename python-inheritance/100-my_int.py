588691be72b73489b75a76ca421a29e11645d917
cgyireh <c.gyireh@alustudent.com>
2023-03-24 12:11:24 -0700

    My integer

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python3
-"""Write a class MyInt that inherits from int """
-
+"""Write a class MyInt that inherits from int"""
+    
 
 class MyInt(int):
+    """Custom int type inverting behavior of != and == operators.
+    """
 
     def __eq__(self, other):
+        """Reverses behavior of == operator.
+        """
         return int(self) != int(other)
 
     def __ne__(self, other):
+        """Reverses behavior of != operator.
+        """
         return int(self) == int(other)
