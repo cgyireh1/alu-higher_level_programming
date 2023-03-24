448252c491fbb2790bc0876f3f9ce73855f0a369
cgyireh <c.gyireh@alustudent.com>
2023-03-24 11:46:58 -0700

    My integer

---
+++ python-inheritance/tests/100-my_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Write a class MyInt that inherits from int """
+
+
+class MyInt(int):
+
+    def __eq__(self, other):
+
+        return int(self) != int(other)
+
+    def __ne__(self, other):
+
+        return int(self) == int(other)
