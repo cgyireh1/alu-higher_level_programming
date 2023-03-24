361f264df5e946145e3cb49d2647a27ab4792447
cgyireh <c.gyireh@alustudent.com>
2023-03-24 10:52:33 -0700

    Search and update

---
+++ python-input_output/100-append_after.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""     
+Contains a function that inserts a line of text to a file.
+"""
+
+
+def append_after(fname="", search_string="", new_string=""):
+    """
+    Inserts a line of text to a file, after
+    each line containing a specific string.
+    """
+    out = ""
+    with open(fname, 'r') as f:
+        for line in f:
+            out += line
+            if search_string in line:
+                out += new_string
+
+    with open(fname, 'w') as f:
+        f.write(out)
