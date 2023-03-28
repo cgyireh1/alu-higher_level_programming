c07db6e87cfbcfd2de557c144bff86200cab6131
cgyireh <c.gyireh@alustudent.com>
2023-03-28 14:44:22 -0700

    Go to UTF8

---
+++ SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,4 @@
+-- import table dump into database
+-- script that displays the average temperature by city ordered by temperature (descending)
+
+SELECT city, AVG(value) AS 'avg_temp' FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
