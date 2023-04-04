fd91cfa37debafbcf86eeade3b4c2ed45aae46ec
cgyireh1 <c.gyireh@alustudent.com>
2023-04-04 02:28:26 +0000

    cURL to the end

--- python-network_0/0-body_size.sh
+++ python-network_0/0-body_size.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#Write a Bash script that takes in a URL, sends a request to that URL
-#Display size of body of response
-#The size must be displayed in bytes
-#You have to use curl
+#script that takes in a URL, sends a request to that URL,and display size of body of response
 curl -sI "$1" | grep 'Content-Length:' | cut -f2 -d' '
--- python-network_0/1-body.sh
+++ python-network_0/1-body.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-#Write a Bash script that takes in a URL, sends a GET request to the URL
-#Displays the body of the response
-#Display only body of a 200 status code response
-#You have to use curl
+#script that takes in a URL, sends a GET request to the URL, and displays the body of the response
 curl -sL "$1"
