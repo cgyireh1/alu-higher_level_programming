874ef43cb1d2b3b6825cc089530162cec3bc909d
cgyireh <c.gyireh@alustudent.com>
2023-04-25 13:25:57 -0700

    Update a state

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,6 +3,8 @@
 updates the name of State with id = 2 to be 'New Mexico'
 using SQLAlchemy and importing State and Base from model_state
 """
+
+
 from sys import argv
 from model_state import Base, State
 
@@ -10,12 +12,12 @@ from sqlalchemy import (create_engine)
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        for instance in session.query(State).filter_by(id=2):
-                instance.name = 'New Mexico'
-        session.commit()
-        session.close()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).filter_by(id=2):
+        instance.name = 'New Mexico'
+    session.commit()
+    session.close()
