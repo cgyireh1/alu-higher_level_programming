619fc0fa9617f44ebc66e53be46169d8f8de073a
cgyireh <c.gyireh@alustudent.com>
2023-04-25 13:17:36 -0700

    All states via SQLAlchemy

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -4,6 +4,7 @@ lists all State objects from the database in ascending order
 using SQLAlchemy and importing State and Base from model_state
 """
 
+
 from sys import argv
 from model_state import Base, State
 
@@ -12,11 +13,11 @@ from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-        engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
             argv[1], argv[2], argv[3]), pool_pre_ping=True)
-        Base.metadata.create_all(engine)
-        Session = sessionmaker(bind=engine)
-        session = Session()
-        for instance in session.query(State).order_by(State.id):
-                print("{}: {}".format(instance.id, instance.name))
-        session.close()
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    for instance in session.query(State).order_by(State.id):
+        print("{}: {}".format(instance.id, instance.name))
+    session.close()
