7481ab29aaf1404e4fe3610d4af65202918e99dc
cgyireh <c.gyireh@alustudent.com>
2023-04-27 13:39:14 -0700

    rectangle

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -87,27 +87,27 @@ class Base():
                     if cls.__name__ == "Square":
                         writer.writerow([o.id, o.size, o.x, o.y])
 
-    @classmethod
-    def load_from_file_csv(cls):
-        """Serializes and deserializes in CSV with
-        same behavior as the JSON serialization/deserialization"""
-        objs = []
-        filename = cls.__name__ + ".csv"
-        try:
-            with open(filename, 'r', newline='') as f:
-                reader = csv.reader(f)
-                for row in reader:
-                    if cls.__name__ == "Rectangle":
-                        dic = {"id": int(row[0]),
-                                "width": int(row[1]),
-                                "height": int(row[2]),
-                                "x": int(row[3]),
-                                "y": int(row[4])}
-                    if cls.__name__ == "Square":
-                        dic = {"id": int(row[0]),
-                                "size": int(row[1]),
-                                "x": int(row[2]),
-                                "y": int(row[3])}
-                        o = cls.create(**dic)
-                        objs.append(o)
-             return objs
+                        @classmethod
+                        def load_from_file_csv(cls):
+                        """Serializes and deserializes in CSV with same behavior
+                        as the JSON serialization/deserialization"""
+                        objs = []
+                        filename = cls.__name__ + ".csv"
+                        try:
+                            with open(filename, 'r', newline='') as f:
+                                reader = csv.reader(f)
+                                for row in reader:
+                                    if cls.__name__ == "Rectangle":
+                                        dic = {"id": int(row[0]),
+                                                "width": int(row[1]),
+                                                "height": int(row[2]),
+                                                "x": int(row[3]),
+                                                "y": int(row[4])}
+                                        if cls.__name__ == "Square":
+                                            dic = {"id": int(row[0]),
+                                                    "size": int(row[1]),
+                                                    "x": int(row[2]),
+                                                    "y": int(row[3])}
+                                            o = cls.create(**dic)
+                                            objs.append(o)
+                               return objs
