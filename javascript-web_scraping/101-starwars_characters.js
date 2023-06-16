a15d4c5e95281b37db78ad83981a0fe8ee3abdc4
cgyireh <c.gyireh@alustudent.com>
2023-06-16 13:34:14 -0700

    edit

--- javascript-web_scraping/101-starwars_characters.js
+++ javascript-web_scraping/101-starwars_characters.js
@@ -1,34 +1,20 @@
 #!/usr/bin/node
 const request = require('request');
-const myArgs = process.argv.slice(2);
-const URLstring = 'https://swapi.dev/api/films/' + myArgs[0];
-
-function characterFunc (filmCharacters, allCharacters, i = 0) {
-  if (i === filmCharacters.length) {
-    return;
-  }
-  const characterUrl = filmCharacters[i];
-  const characterIndex = allCharacters.findIndex(c => c.url === characterUrl);
-  if (characterIndex >= 0) {
-    console.log(allCharacters[characterIndex].name);
-  } else {
-    console.log('Character not found');
-  }
-  characterFunc(filmCharacters, allCharacters, i + 1);
-}
-
-request(URLstring, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else {
-    const filmCharacters = JSON.parse(body).characters;
-    request('https://swapi.dev/api/people', function (err, response, body) {
-      if (err) {
-        console.log(err);
-      } else {
-        const allCharacters = JSON.parse(body).results;
-        characterFunc(filmCharacters, allCharacters);
-      }
-    });
+const url = 'https://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (!error) {
+    let characters = JSON.parse(body).characters;
+    printCharacters(characters, 0);
   }
 });
+
+function printCharacters (characters, index) {
+  request(characters[index], function (error, response, body) {
+    if (!error) {
+      console.log(JSON.parse(body).name);
+      if (index + 1 < characters.length) {
+        printCharacters(characters, index + 1);
+      }
+    }
+  });
+}
