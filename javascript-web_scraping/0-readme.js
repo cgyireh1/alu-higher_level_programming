35d02007cc04cc16d60a797e0e6ffbbc3b943fd0
cgyireh <c.gyireh@alustudent.com>
2023-06-16 08:37:16 -0700

    javascript-web_scraping

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let filename = process.argv[2];
+const filename = process.argv[2];
 const fs = require('fs');
 fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
@@ -9,3 +9,4 @@ fs.readFile(filename, 'utf8', function (err, data) {
     console.log(data);
   }
 });
+
