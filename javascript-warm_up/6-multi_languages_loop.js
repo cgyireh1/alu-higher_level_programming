afa8771255073ea2fcb829d6d7c0b84998f4c56c
cgyireh <c.gyireh@alustudent.com>
2023-06-16 10:27:53 -0700

    edits

--- javascript-warm_up/6-multi_languages_loop.js
+++ javascript-warm_up/6-multi_languages_loop.js
@@ -1,4 +1,4 @@
 #!/usr/bin/node
-for (const l of ['C is fun', 'Python is cool', 'JavaScript is amazing']){
-	console.log(l);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
